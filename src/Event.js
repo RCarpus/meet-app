01867a6a8ace0b35e829944ed783626080055fc9
RCarpus <40838987+RCarpus@users.noreply.github.com>
2021-12-13 10:16:25 -0500

    Add unit tests for Event element

--- src/Event.js
+++ src/Event.js
@@ -6,44 +6,47 @@ class Event extends Component {
     super();
 
     this.state = {
-      expanded: false
+      expanded: false,
+      eventDetailsButtonText: 'More details'
     };
   }
 
   handleClickDetailsButton = (e) => {
     const current = this.state.expanded;
     this.setState({
-      expanded: current ? false : true
+      expanded: current ? false : true,
+      eventDetailsButtonText: current ? 'More details' : 'Hide details'
     });
   }
 
   render() {
-    const { expanded } = this.state;
-    const { event }= this.props;
-    return ( 
-    <div className='event'>
-      <h2 className='event__name'>{event.summary}</h2>
-      <p className='event__start'></p>
-      <p className='event__title-line-2'>
-        <span className='event__title-line-2__at-sign'>@</span>
-        <span className='event__title-line-2__title'></span>
-        <span className='event__title-line-2__pipe'> | </span>
-        <span className='event__title-line-2__city'></span>
-      </p>
-
-      { expanded && 
-      <div className='event__more-details' >
-        <h3 className='event__more-details__about-label'>About event</h3>
-        <p className='event__more-details__link-line'>
-          <a className='event__more-details__link-line__link'></a>
+    const { expanded, eventDetailsButtonText } = this.state;
+    const { event } = this.props;
+    return (
+      <div className='event'>
+        <h2 className='event__name'>{event.summary}</h2>
+        <p className='event__start'>{event.start.dateTime}</p>
+        <p className='event__title-line-2'>
+          <span className='event__title-line-2__at-sign'>@</span>
+          <span className='event__title-line-2__title'>{event.summary}</span>
+          <span className='event__title-line-2__pipe'> | </span>
+          <span className='event__title-line-2__city'>{event.location}</span>
         </p>
-        <p className='event__more-details__description'></p>
-      </div>
-      }
 
-      <button className='event__details-button' 
-              onClick={(e) => this.handleClickDetailsButton(e)}></button>
-    </div>
+        {expanded &&
+          <div className='event__more-details' >
+            <h3 className='event__more-details__about-label'>About event</h3>
+            <p className='event__more-details__link-line'>
+              <a className='event__more-details__link-line__link'>{event.htmlLink}</a>
+            </p>
+            <p className='event__more-details__description'>{event.description}</p>
+          </div>
+        }
+
+        <button className='event__details-button'
+          onClick={(e) => this.handleClickDetailsButton(e)}>{eventDetailsButtonText}</button>
+
+      </div>
     );
   }
 }
