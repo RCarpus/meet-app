0dc592f0a0d828f24b56bd210000175b0f860f76
RCarpus <40838987+RCarpus@users.noreply.github.com>
2021-12-13 11:10:59 -0500

    Implemented NumberOfEvents with unit tests

---
+++ src/NumberOfEvents.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+// import './css/NumberOfEvents.css';
+
+class NumberOfEvents extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      number: 32
+    };
+  }
+
+  handleInputChanged = (event) => {
+    const value = this.RemoveNonNumeric(event.target.value);
+    this.setState({
+      number: value,
+    });
+  }
+
+  RemoveNonNumeric = (text) => {
+    return text.replace(/[^0-9]/g, '');
+  }
+
+
+  render() {
+    return (
+      <div className="number-of-events">
+        <label id="number-of-events__label" for="number-of-events__input">Number of Events:</label>
+        <input id="number-of-events__input" value={this.state.number} onChange={this.handleInputChanged}/>
+      </div>
+    )
+  }
+}
+
+export default NumberOfEvents;
