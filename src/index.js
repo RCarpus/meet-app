ba79428dd48c2047a4d08a77234706404ba96e6f
RCarpus <40838987+RCarpus@users.noreply.github.com>
2021-12-15 13:58:24 -0500

    Connected Atatus to app

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+// ---------Monitoring with Atatus --------------
+import * as atatus from 'atatus-spa';
+atatus.config('9d78ec172e60442196c0bde0ec64e257').install();
+// ----------------------------------------------
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,6 +15,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
