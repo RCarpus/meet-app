4849b94ce1bb0268432ed1b3b3e66cb4773f2c16
RCarpus <40838987+RCarpus@users.noreply.github.com>
2021-12-16 10:22:16 -0500

    implemented ErrorAlert

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
-// import './css/NumberOfEvents.css';
+import { ErrorAlert } from './alert';
 
 class NumberOfEvents extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      number: 32
+      number: 32,
+      errorText: ''
     };
   }
 
   handleInputChanged = (event) => {
-    const value = this.RemoveNonNumeric(event.target.value);
-    this.setState({
-      number: value,
-    });
-    
-    //unit test for NumberOfEvents fails if it tries to run this function
+    let value = event.target.value;
+    let cleanedValue = this.RemoveNonNumeric(value);
+
+    if (value !== cleanedValue) {
+      console.log('clenaed avlue again', cleanedValue);
+      this.setState({
+        number: cleanedValue,
+        errorText: 'Number of Events must be a non-negative integer'
+      })
+    } else {
+      this.setState({
+        number: cleanedValue,
+        errorText: ''
+      });
+    }
+
     if (this.props.updateNumberOfEvents)
-      this.props.updateNumberOfEvents(value);
+      this.props.updateNumberOfEvents(cleanedValue);
 
   }
 
@@ -30,8 +41,13 @@ class NumberOfEvents extends Component {
   render() {
     return (
       <div className="number-of-events">
-        <label id="number-of-events__label" htmlFor="number-of-events__input">Number of Events:</label>
-        <input id="number-of-events__input" value={this.state.number} onChange={this.handleInputChanged} />
+        <div className="number-of-events__error">
+          <ErrorAlert text={this.state.errorText} />
+        </div>
+        <div className="number-of-events__grid">
+          <label id="number-of-events__label" htmlFor="number-of-events__input">Number of Events:</label>
+          <input id="number-of-events__input" value={this.state.number} onChange={this.handleInputChanged} />
+        </div>
       </div>
     )
   }
