a3696474b8b8042abff6fe08f259ebe31f142b55
RCarpus <40838987+RCarpus@users.noreply.github.com>
2021-12-17 11:22:05 -0500

    set up service worker and added icons

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import * as atatus from 'atatus-spa';
 atatus.config('9d78ec172e60442196c0bde0ec64e257').install();
 // ----------------------------------------------
 
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -20,7 +21,7 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
