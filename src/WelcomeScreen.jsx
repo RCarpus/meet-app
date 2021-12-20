3078c538d6d0f868498dc070c5c49d5a368c5613
RCarpus <40838987+RCarpus@users.noreply.github.com>
2021-12-20 08:55:00 -0500

    Added app and brand info to WelcomeScreen

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -27,6 +27,24 @@ function WelcomeScreen(props) {
             >
               <b>Sign in with google</b>
             </button>
+            <div className="WelcomeScreen__app-info">
+              <h2>What does this app do, and why do I need to sign in with my Google account?</h2>
+              <p>
+                Meet app is a Progressive Web App (PWA) built with React and coded using a test-driven development process. The app uses the Google Calendar API to fetch upcoming events from a a CareerFoundry events calendar.
+              </p>
+              <p>
+                Once logged in, the app is a single page app with two inputs, one for a city name, and one for a number of events to show. Simply input your desired city and select it from the suggestions dropdown, then narrow down or increase the number of events shown with using the number input. Clicking on the "more details" button for an event will display a description and URL for the event. Clicking the event URL will redirect you to the Google Calendar event.
+              </p>
+              <p>
+                The Meet app is using https://www.googleapis.com/auth/calendar.events.readonly for
+                educational purposes only. The calendar accessed isn’t the user’s but a calendar provided by
+                CareerFoundry as part of their Full-Stack Immersion course. No user information is saved or
+                used within the application, and personal calendars aren’t accessed.
+              </p>
+              <p>
+                If you'd like to see more of my work, please visit <a href="http://rcarpus.github.io">my portfolio site at rcarpus.github.io</a>.
+              </p>
+            </div>
           </div>
         </div>
         <a
